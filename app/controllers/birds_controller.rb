37901baa3f3b2a16744a3e045e05fa45724058ab
Mohamed <mohasalanka2020@gmail.com>
2023-06-20 20:27:17 +0300

    finished on RR-index-show

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,14 @@
+class BirdsController < ApplicationController
+    def index
+      birds = Bird.all
+      render json: birds
+    end
+  
+    def show
+      bird = Bird.find(params[:id])
+      render json: bird
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+end
+  
